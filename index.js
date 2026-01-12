78af50c533ca3298d566f9966162a9ac1c33467f
Om Vigave <sohamvigave@gmail.com>
2026-01-12 16:31:16 +0530

    basic setUp

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const mongoose = require("mongoose");
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
+main()
+.then(res => {
+    console.log("connectio successful");
+})
+.catch(err => {
+    console.log(err);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+app.listen(port,(req, res) => {
+    console.log(`listening on ${port}`);
+});
