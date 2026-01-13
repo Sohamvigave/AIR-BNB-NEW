3f25f76601c7655a6ed1f75920bb08f0535d80e4
Om Vigave <sohamvigave@gmail.com>
2026-01-13 11:27:57 +0530

    listing model

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const Listing = require("./models/listings.js");
 
 async function main() {
     await mongoose.connect(MONGO_URL);
@@ -18,6 +19,18 @@ main()
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
+});
+
+app.get("/testListing", (req, res) => {
+    let sampleListing = new Listing({
+        title: "my new villa",
+        discription: "by the beach",
+        price: 5550,
+        location: "new delhi",
+        country: "india"
+    });
+    sampleListing.save();
+    res.send("testListing");
 });
 
 app.listen(port,(req, res) => {
